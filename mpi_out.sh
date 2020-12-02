b6ebe5896eb177c89063c26573ace3b730acdfb2
Victor David Francisco  Cavero Huapaya <victor.cavero@khipu.utec.edu.pe>
2020-12-01 20:21:49 -0500

    changes in cluster

---
+++ mpi_out.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH -J CPD-test-16:07:58-12/01/20
+#SBATCH -p investigacion
+#SBATCH -N 1
+#SBATCH --tasks-per-node=8
+
+module load openmpi/2.1.6
+
+echo "1 Proceso"
+for i in {0...100}
+do
+	mpirun -np 1 random_mpi.out
+done
+
+echo "2 Procesos"
+for i in {0...100}
+do
+        mpirun -np 2 random_mpi.out
+done
+
+echo "4 Procesos"
+for i in {0...100}
+do
+        mpirun -np 4 random_mpi.out
+done
+
+echo "8 Procesos"
+for i in {0...100}
+do
+        mpirun -np 8 random_mpi.out
+done
+
+#echo "12 Procesos"
+#for i in {0...100}
+#do
+#        mpirun -np 12 random_mpi.out
+#done
+
+
+module unload openmpi/2.1.6
---
+++ omp_out.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH -J CPD-test-16:27:24-12/01/20
+#SBATCH -p investigacion
+#SBATCH -N 1
+#SBATCH --tasks-per-node=8
+
+module load gcc/5.5.0
+
+echo "1 Proceso"
+for i in {1..100}
+do
+	./random_openmp.out 1
+done
+
+echo "2 Procesos"
+for i in {1..100}
+do
+        ./random_openmp.out 2
+done
+
+echo "4 Procesos"
+for i in {1..100}
+do
+        ./random_openmp.out 4
+done
+
+echo "8 Procesos"
+for i in {1..100}
+do
+        ./random_openmp.out 8
+done
+
+#echo "12 Procesos"
+#for i in {1..100}
+#do
+ #       ./random_openmp.out 12
+#done
+
+
+
+module unload gcc/5.5.0
